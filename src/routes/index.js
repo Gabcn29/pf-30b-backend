e3818ee43f02dfdc4af8f2442a5a60e897f5631e
Franco <juanfrancorodriguez99@gmail.com>
2022-11-24 16:29:19 -0300

    /articulo/rellenarBase

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const articulos = require("./articulos.js");
 const category = require("./category.js");
 const users = require("./users.js");
+const mp = require("./mp.js");
 const router = express.Router();
 
 router.use("/articulo", articulos);
 router.use("/category", category);
 router.use("/users", users);
+router.use("/mercadoPago", mp);
 
 module.exports = router;
