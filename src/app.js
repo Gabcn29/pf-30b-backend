cc45d42f8441724215906a247e7d3183e70c81fc
Franco <juanfrancorodriguez99@gmail.com>
2022-11-28 15:02:47 -0300

    asd

--- src/app.js
+++ src/app.js
@@ -17,12 +17,9 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(cors());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from, thank you this make me lost a lot of time
+  res.header("Access-Control-Allow-Origin", "https://ecommerce-frontend-30b.vercel.app/"); // update to match the domain you will make the request from, thank you this make me lost a lot of time
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
