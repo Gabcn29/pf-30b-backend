0af3f2c54115c60668e906c2a859b56d7c732441
Federico Montanari <fedemontanari@outlook.com>
2022-12-06 19:56:20 -0300

    Minor changes

--- src/db.js
+++ src/db.js
@@ -14,7 +14,10 @@ const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, "/models"))
-  .filter((file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js")
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
@@ -23,12 +26,24 @@ fs.readdirSync(path.join(__dirname, "/models"))
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Article, Category, User, Factura, Billitems, Cartitems, Wishlist } = sequelize.models;
+const {
+  Article,
+  Category,
+  User,
+  Factura,
+  Billitems,
+  Cartitems,
+  Wishlist,
+  Review,
+} = sequelize.models;
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
@@ -50,8 +65,8 @@ User.belongsToMany(Article, { through: Wishlist });
 Article.belongsTo(User, { through: Wishlist });
 
 // Para las reseñas
-Review.belongsTo(Article, {through: "ArticleReview"})
-Article.hasMany(Review)
+Review.belongsTo(Article, { through: "ArticleReview" });
+Article.belongsToMany(Review, { through: "ArticleReview" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
